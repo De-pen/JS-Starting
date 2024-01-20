4dfeae49ede4592573e0cba7f8a77acf87900098
De-pen <155830739+De-pen@users.noreply.github.com>
2024-01-20 16:28:22 +0000

    "Different data types in JS"

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+let dance = false //This is boolean data type
+let address = "USA" //This is string data types
+let state; //This is undefined data type
+let place = null //This is object
+console.table([typeof dance,address,state,place])
