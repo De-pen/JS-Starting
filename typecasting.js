052862153f2a4a0bd7e5f2b964c92ec15694cf89
De-pen <155830739+De-pen@users.noreply.github.com>
2024-01-20 17:10:40 +0000

    Type casting with various data types on integer and boolean

---
+++ typecasting.js
@@ -0,0 +1,24 @@
+/*
+let score = undefined
+numscore = Number(score)
+console.log(typeof(numscore))
+console.log((numscore))
+*/
+
+// score = "33"=> number
+// score = "33abc" => Nan(Not a number)
+// score = null => zero
+// score = true => 1
+//score = false => 0
+//score undefined => NaN
+let play = undefined
+boolplay = Boolean(play)
+console.log(boolplay)
+console.log(typeof (boolplay))
+// play = 1 => True
+// play = 0 => False
+//play = ""c=>Fasle
+//play = " " => True
+//play = "Depen" =>True
+//play = null =>False
+//play = undefined  =>False
